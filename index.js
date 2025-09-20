47bb86436b127c3c549ee5afdb3294c151a9ae83
mrolystone-art <mrolystone@gmail.com>
2025-09-20 09:09:37 +0000

    no changes committed

---
+++ index.js
@@ -0,0 +1,19 @@
+function sum(a,b){
+const results=a+b
+return results
+}
+let score=sum(3,2)
+console.log(score)
+
+
+
+function get(num){
+	if(num%2===0){
+	console.log(`${num} is even`)
+	}
+    else{
+		console.log(`${num} is odd`)
+	}
+}
+
+get(3)
